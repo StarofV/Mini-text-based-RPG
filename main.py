239efbd83d771cf7883bea0faa2e98e6c6c6115a
DogeSec <utdoge@gmail.com>
2017-09-20 08:43:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#Some RPG game, made by (DogeSec™), enjoy :D 
+#Some RPG game, made by (DogeSec™), enjoy :D.. In devolpment updated 9/20/17
+
+from time import sleep
 
 name = input("Enter your name\n")
 
@@ -12,7 +14,7 @@ def menu():
         ''')
   
   user_choice = input()
-  if user_choice == '1': #continue into the story
+  if user_choice == '1':
     roads()
   elif user_choice == '2': #exit game if chosen
     exit(2) #exit without erros
@@ -21,15 +23,34 @@ def menu():
 
 def roads(): #Road scene
   road_choice = input("there are 2 roads infront of you, which one will you choose? L/R")
+  
   if road_choice.lower() in ('r'): # checking if user choice "R"
     right_road()
-
-def creds():
-  print("By DogeSec™, and others to come") 
+  
+  elif road_choice.lower() in ('l'):
+    print("You made it into level 2")
+    left_road()
+    
+def Game_Over():
+  print("You lost, you will be returning to menu in 5 seconds")
+  sleep(5)
   menu()
-
-
+  
+def creds():
+  print("By DogeSec™, and others to come?...")
+  menu()
+  
+def left_road():
+  welcome = input("Welcome to the next level, move on? Y/N\n")
+  if welcome.lower() in ('y'):
+    print("This is the end to the mini RPG (not even an RPG, lol)")
+    sleep(2)
+    creds()
+  else: 
+    print("Bye")
+    sleep(3)
+    creds()
+    exit()
 def right_road(): #The right road choice 
-  print("You lost") #In devolpment, will be changed 
-
-menu()
+  print("Game Over\n") 
+  Game_Over()
